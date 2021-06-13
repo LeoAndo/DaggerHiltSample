7ef3ce283117a0d436abdb9b1a107d3863159370
Reo Ando <ando.reo420@gmail.com>
2021-06-13 22:48:53 +0900

    #3 daggerHiltを2.37にバージョンアップする

--- app/src/main/java/com/template/daggerhiltsample/di/ServiceModule.kt
+++ app/src/main/java/com/template/daggerhiltsample/di/ServiceModule.kt
@@ -5,14 +5,14 @@ import com.template.daggerhiltsample.TestServiceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.components.SingletonComponent
 
 // https://developer.android.com/training/dependency-injection/hilt-android#inject-interfaces
 // @Binds を使用してインターフェース インスタンスを注入する
 // https://developer.android.com/training/dependency-injection/hilt-android#component-lifetimes
 // 各コンポーネントの生存期間については上記参照.
 @Module
-@InstallIn(ApplicationComponent::class) // アプリケーションスコープ
+@InstallIn(SingletonComponent::class) // アプリケーションスコープ
 abstract class ServiceModule {
     @Binds
     abstract fun bindTestService(testServiceImpl: TestServiceImpl): TestService
--- app/src/main/java/com/template/daggerhiltsample/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/template/daggerhiltsample/ui/home/HomeViewModel.kt
@@ -1,23 +1,17 @@
 package com.template.daggerhiltsample.ui.home
 
-import android.content.Context
 import android.util.Log
-import android.widget.Toast
-import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.template.daggerhiltsample.TestService
-import dagger.hilt.android.qualifiers.ActivityContext
-import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 // https://developer.android.com/training/dependency-injection/hilt-android#predefined-qualifiers
 // @ApplicationContext , @ActivityContext については上記参照.
-class HomeViewModel @ViewModelInject constructor(
-    @ApplicationContext private val applicationContext: Context,
-    @ActivityContext private val activityContext: Context,
-    testService: TestService
-) : ViewModel() {
+@HiltViewModel
+class HomeViewModel @Inject constructor(testService: TestService) : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
@@ -25,10 +19,6 @@ class HomeViewModel @ViewModelInject constructor(
     val text: LiveData<String> = _text
 
     init {
-        Log.w(
-            "andoleo",
-            "applicationContext: $applicationContext activityContext: $activityContext"
-        )
-        Toast.makeText(activityContext, testService.getText(), Toast.LENGTH_LONG).show()
+        Log.d("HomeViewModel", "testService.getText(): ${testService.getText()}")
     }
 }
